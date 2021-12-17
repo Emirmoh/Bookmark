81e9e50eef23e18417101537b844579ddc6ab095
Mohamed Emir Bouhamar <87539839+Emirmoh@users.noreply.github.com>
2021-12-17 16:55:47 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*=============== SHOW MENU ===============*/
+const toggle = document.getElementById("toggle");
+const links = document.getElementById("links");
+const close = document.getElementById("close");
+toggle.addEventListener("click", () => links.classList.add("active"));
+close.addEventListener("click", () => links.classList.remove("active"));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const header = document.querySelector("header");
+window.addEventListener("scroll", chngBg);
+function chngBg() {
+  if (this.scrollY >= 50) header.classList.add("scroll");
+  else header.classList.remove("scroll");
+}
+
+/*=============== FEATURES TABS ===============*/
+const btns = document.querySelectorAll(".features .content .filter li");
+const tabs = document.querySelectorAll(".features .content .tab");
+
+btns.forEach((btn, idx) =>
+  btn.addEventListener("click", () => {
+    btns.forEach((btn) => btn.classList.remove("active"));
+    btn.classList.add("active");
+    tabs.forEach((tab) => tab.classList.add("hidden"));
+    tabs[idx].classList.remove("hidden");
+  })
+);
+/*=============== FAQs SHOW/HIDE ===============*/
+const faqs = document.querySelectorAll(".faq .content ul li");
+const toggles = document.querySelectorAll(
+  ".faq .content ul li .qst-container .toggle"
+);
+
+toggles.forEach((toggle, i) =>
+  toggle.addEventListener("click", () => {
+    faqs.forEach((toggle, j) => {
+      if (i != j) toggle.classList.remove("active");
+    });
+    faqs[i].classList.toggle("active");
+  })
+);
